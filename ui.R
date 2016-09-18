b9438e1b4e2b69fdecf415989760497b9cf0fddb
spacedinosaur95 <rebeccalorraine.demarco@gmail.com>
2016-09-18 16:43:51 -0400

    Update ui.R
    converted from just a shiny app to shiny dashboard...

--- ui.R
+++ ui.R
@@ -1,31 +1,48 @@
-
 # This is the user-interface definition of a Shiny web application.
 # You can find out more about building applications with Shiny here:
-#
 # http://shiny.rstudio.com
-#
 
 library(shiny)
 library(gdata)
-wakeupdemo = read.csv("demodata.csv")
+library(shinydashboard)
+wakeupdemo <- read.csv("demodata.csv")
 
-shinyUI(fluidPage(
+skin <- Sys.getenv("DASHBOARD_SKIN")
+skin <- tolower(skin)
+if (skin == "")
+  skin <- "blue"
+header <- dashboardHeader(title = "Space Shuttle Wakeup Calls")
+sidebar <- dashboardSidebar(
+  menuItem("Explore by Mission", tabName = "explorebymission", icon = icon("space-shuttle")),
+  menuItem("Data Vis", tabName = "datavis", icon = icon("pie-chart")),
+  menuItem("Connections", tabName = "connections", icon = icon("exchange"))
+)
 
-  # Application title
-  titlePanel("Shuttle Wakeup Songs"),
-
-
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("dataset", "Choose a dataset:", 
-                  # put in options for the dropdown box
-                 as.character(unique(wakeupdemo$Mission))
-    )
-),
+body <- dashboardBody(
+  box(
+    title = "Select Mission", status = "primary", solidHeader = TRUE,
+    collapsible = FALSE,
+    selectInput("dataset", "Choose a dataset:",
+                # put in options for the dropdown box
+                as.character(unique(wakeupdemo$Mission)))
+  ),
+  box(
+    title = "Songs Played on Mission", status = "warning", solidHeader = TRUE,
     # Show a plot of the generated distribution
-    mainPanel(
-      DT::dataTableOutput("table")
-    )
+    mainPanel(DT::dataTableOutput("table")))
   )
-))
+ui <- dashboardPage(header, sidebar, body, skin = skin)
+# 
+# shinyUI(fluidPage(
+#   # Application title
+#   titlePanel("Shuttle Wakeup Songs"),
+#   
+#   # Sidebar with a slider input for number of bins
+#   sidebarLayout(sidebarPanel(
+#     selectInput("dataset", "Choose a dataset:",
+#                 # put in options for the dropdown box
+#                 as.character(unique(wakeupdemo$Mission)))
+#   ),
+#   # Show a plot of the generated distribution
+#   mainPanel(DT::dataTableOutput("table")))
+# ))
