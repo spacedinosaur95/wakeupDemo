0e72dabd41825391d7f7c2ad1a20cc2cf53f3245
spacedinosaur95 <rebeccalorraine.demarco@gmail.com>
2016-09-19 12:01:20 -0500

    Update ui.R
    made my menuItems in the sidebar work, put in a tabBox for the Data Vis menuItem and call in the plots from the server

--- ui.R
+++ ui.R
@@ -5,6 +5,7 @@
 library(shiny)
 library(gdata)
 library(shinydashboard)
+library(plotly)
 wakeupdemo <- read.csv("demodata.csv")
 
 skin <- Sys.getenv("DASHBOARD_SKIN")
@@ -12,37 +13,64 @@ skin <- tolower(skin)
 if (skin == "")
   skin <- "blue"
 header <- dashboardHeader(title = "Space Shuttle Wakeup Calls")
-sidebar <- dashboardSidebar(
-  menuItem("Explore by Mission", tabName = "explorebymission", icon = icon("space-shuttle")),
-  menuItem("Data Vis", tabName = "datavis", icon = icon("pie-chart")),
-  menuItem("Connections", tabName = "connections", icon = icon("exchange"))
-)
-
-body <- dashboardBody(
-  box(
-    title = "Select Mission", status = "primary", solidHeader = TRUE,
-    collapsible = FALSE,
-    selectInput("dataset", "Choose a dataset:",
-                # put in options for the dropdown box
-                as.character(unique(wakeupdemo$Mission)))
+sidebar <- dashboardSidebar(sidebarMenu(
+  menuItem(
+    "Explore by Mission",
+    tabName = "explorebymission",
+    icon = icon("space-shuttle")
   ),
-  box(
-    title = "Songs Played on Mission", status = "warning", solidHeader = TRUE,
-    # Show a plot of the generated distribution
-    mainPanel(DT::dataTableOutput("table")))
+  menuItem("Data Vis", tabName = "datavis", icon = icon("pie-chart")),
+  menuItem(
+    "Connections",
+    tabName = "connections",
+    icon = icon("exchange")
   )
+))
+
+body <- dashboardBody(tabItems(
+  tabItem(
+    tabName = "explorebymission",
+    box(
+      title = "Select Mission",
+      status = "primary",
+      solidHeader = TRUE,
+      collapsible = FALSE,
+      selectInput("dataset", "Choose a dataset:",
+                  # put in options for the dropdown box
+                  as.character(unique(wakeupdemo$Mission)))
+    ),
+    box(
+      title = "Songs Played on Mission",
+      status = "warning",
+      solidHeader = TRUE,
+      # Show a plot of the generated distribution
+      mainPanel(DT::dataTableOutput("table"))
+    )
+  ),
+  
+  tabItem(tabName = "datavis",
+          fluidRow(
+            tabBox(
+              title = "Plotly tabBox",
+              id = "tabset1",
+              height = "500px",
+              width = "450px",
+              tabPanel(
+                "Bar Chart",
+                h2("plotly demo"),
+                box(title = "Bar Chart", status = "primary",
+                    plotlyOutput('plot1'))
+              ),
+              
+              tabPanel(
+                "Pie Chart",
+                h2("plotly demo"),
+                box(title = "Pie Chart", status = "primary",
+                    plotlyOutput('plot2'))
+              )
+            )
+          )),
+  tabItem(tabName = "connections",
+          h2("Neo4j Embed Demo"))
+))
 ui <- dashboardPage(header, sidebar, body, skin = skin)
-# 
-# shinyUI(fluidPage(
-#   # Application title
-#   titlePanel("Shuttle Wakeup Songs"),
-#   
-#   # Sidebar with a slider input for number of bins
-#   sidebarLayout(sidebarPanel(
-#     selectInput("dataset", "Choose a dataset:",
-#                 # put in options for the dropdown box
-#                 as.character(unique(wakeupdemo$Mission)))
-#   ),
-#   # Show a plot of the generated distribution
-#   mainPanel(DT::dataTableOutput("table")))
-# ))
